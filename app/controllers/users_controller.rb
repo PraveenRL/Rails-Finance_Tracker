e5e2770e8b678c9e7e569198630911ca6eebbcde
PraveenRL <rlppraveen@gmail.com>
2020-05-05 14:04:47 +0530

    Friends List

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
     @tracked_stocks = current_user.stocks #helper from devise, refer in gem devise
   end
 
+  def my_friends
+    @friends = current_user.friends
+  end
+
 end
